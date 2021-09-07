ddc64ea4d12dca2d37718124fa0978e5a9b011e1
Kishan Tiwari <64146963+kishanmanitiwari@users.noreply.github.com>
2021-09-07 14:59:24 +0530

    Update benjaminBulb.java

--- Getting_Started/benjaminBulb.java
+++ Getting_Started/benjaminBulb.java
@@ -14,9 +14,9 @@
    }
   }
 
- //Approach-> Initially all the bulbs  are off so when the flctauation occurs only those bulb will remain on
-//  having odd flctauations. And all perfect sqaures  have odd fluctations becasuse there factor comes out to be 2n+1 factors... 
+ //Approach-> Initially all the bulbs  are off so when the flctauation occurs only those bulb will remain on having odd flctauations. 
+// And all perfect sqaures  have odd fluctations becasuse there factor comes out to be 2n+1 factors... 
 
 
-//Eg - 36-> have 9 fators so odd and why odd matters because on every even fluctuation the bulb will come to its orignal state..
-// Eg-4(Perfect Square) -> 1->ON 2-> OFF 3-> 4-> ON and 4 has 2n+1 factors that is odd.
+//Eg - 36-> have 9 factors so odd and why odd matters because on every even fluctuation the bulb will come to its orignal state..
+// Eg-4(Perfect Square) -> 1->ON 2-> OFF  4-> ON and 4 has 2n+1 factors that is odd.
