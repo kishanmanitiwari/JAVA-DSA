808888e51b24e3eba1a3bdc9646d2bbf0924af0e
Kishan Tiwari <64146963+kishanmanitiwari@users.noreply.github.com>
2021-09-07 14:57:58 +0530

    Add files via upload

---
+++ Getting_Started/benjaminBulb.java
@@ -0,0 +1,22 @@
+ import java.util.*;
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    
+    int n = scn.nextInt();
+    
+    for(int i=1;i<=(int)Math.sqrt(n);i++) //Loop goes till nth root
+    System.out.println(i * i); //Print Suare of the ith root 
+        
+
+   }
+  }
+
+ //Approach-> Initially all the bulbs  are off so when the flctauation occurs only those bulb will remain on
+//  having odd flctauations. And all perfect sqaures  have odd fluctations becasuse there factor comes out to be 2n+1 factors... 
+
+
+//Eg - 36-> have 9 fators so odd and why odd matters because on every even fluctuation the bulb will come to its orignal state..
+// Eg-4(Perfect Square) -> 1->ON 2-> OFF 3-> 4-> ON and 4 has 2n+1 factors that is odd.
