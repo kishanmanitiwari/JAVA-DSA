0ef1b786c5779aecf24a941c9964fc37e7a89694
Kishan Tiwari <64146963+kishanmanitiwari@users.noreply.github.com>
2021-09-07 15:58:49 +0530

    Add files via upload

---
+++ Getting_Started/Math.Max()Demo.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+
+        int a = 25;
+        int b = 23;
+  
+        // prints the maximum of two numbers
+        System.out.println(Math.max(a, b));
+
+	}
+}
+
