f8062e26b177015d38f5d3a12466ca17a844e593
Kishan Tiwari <64146963+kishanmanitiwari@users.noreply.github.com>
2021-09-07 16:08:40 +0530

    Add files via upload

---
+++ Patterns/pattern1_Increasing.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+
+		for(int i=1;i<=n;i++){ //5  //Runs 1-5 (5 times)
+
+
+            for(int j=1;j<=i;j++){ //Controls Row for ith num eg if outer loop has 3 then j wil run till 3
+
+                System.out.print("*\t"); //\t stands for tab used to give relatively large space
+            }
+
+            System.out.println(); //Go to next row after each intreation!
+
+        }
+
+
+	}
+}
+
