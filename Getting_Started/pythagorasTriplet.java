1c3700bfa561a345b7cccb88a66c05a2ccef46bb
Kishan Tiwari <64146963+kishanmanitiwari@users.noreply.github.com>
2021-09-07 15:56:13 +0530

    Add files via upload

---
+++ Getting_Started/pythagorasTriplet.java
@@ -0,0 +1,45 @@
+ 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+        Scanner fs = new Scanner(System.in);
+		int n1=fs.nextInt();
+        int n2=fs.nextInt();
+        int n3=fs.nextInt();
+
+        //Part 1 -> Fimd max that acts as hypo
+
+    int max = n1;
+
+    if(n2>max)max=n2;
+    if(n3>max)max=n3;
+
+
+    // PPart 2-> Determine Hypo and evaluate condtion using a flag
+
+    boolean flag = false;
+
+    if(max==n1) flag=((n2*n2 + n3*n3)==(max*max));
+
+    else if(max==n2) flag=((n1*n1 + n3*n3)==(max*max));
+
+    else  flag=((n1*n1 + n2*n2)==(max*max));
+       
+    
+
+
+    System.out.println(flag);
+
+
+
+
+	}
+}
+
