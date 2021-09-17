02af04bd2be29da1cda5e807388f58b6f89a1679
Kishan Tiwari <64146963+kishanmanitiwari@users.noreply.github.com>
2021-09-17 14:13:38 +0530

    Update pattern13_pnc(tricky).java

--- Patterns/pattern13_pnc(tricky).java
+++ Patterns/pattern13_pnc(tricky).java
@@ -12,7 +12,7 @@ class Main
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
 
-    for (int i = 0; i < n; i++) {  start loop from 0 becasue for each row 1st col is 1 so nc0 =1
+    for (int i = 0; i < n; i++) { //start loop from 0 becasue for each row 1st col is 1 so nc0 =1
       int val = 1;
       for (int j = 0; j <= i; j++) {
 
